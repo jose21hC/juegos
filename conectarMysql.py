3e2f2e8ad3fac58a3cdde01270bef4e57ad5b8eb
jose21hC <jose.cruzsarabia@cesunbc.edu.mx>
2024-10-18 18:26:07 -0700

    Create conectarMysql.py

---
+++ conectarMysql.py
@@ -0,0 +1,6 @@
+Importar  conector mysql​
+
+cnx  =  mysql .connector .connect ( usuario = 'scott' , contraseña = ' tiger ' ,
+                              host = '127.0.0.1' ,
+                              base de datos = 'empleados' )
+cnx .cerrar ( )
